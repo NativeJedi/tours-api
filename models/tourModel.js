0c7748dd03d54ca221596528fa1d1690c76afc62
NativeJedi <ruslan201010@gmail.com>
2024-08-17 14:33:21 +0300

    fix: debug

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const { Schema } = mongoose;
 
@@ -9,7 +10,10 @@ const toursSchema = new Schema(
       required: [true, 'Name is required'],
       unique: true,
       trim: true,
+      maxlength: [40, 'Tour name must be less than 40 characters'],
+      minLength: [10, 'Tour name must be more than 10 characters'],
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -21,10 +25,16 @@ const toursSchema = new Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty can be easy, medium or difficult',
+      },
     },
     ratingAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be more than 1'],
+      max: [5, 'Rating must be below than 5'],
     },
     ratingsQuantity: {
       type: Number,
@@ -38,7 +48,16 @@ const toursSchema = new Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // this only point on DOC when .create()
+          return val < this.price;
+        },
+        message: 'Discount ({VALUE}) should be less then price',
+      }
+    },
     summary: {
       type: String,
       trim: true,
@@ -65,6 +84,13 @@ const toursSchema = new Schema(
     toObject: { virtuals: true },
   },
 );
+
+// DOCUMENTS MIDDLEWARE run ONLY for .save and .create but not for .update()
+toursSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
 
 toursSchema.virtual('durationInWeeks').get(function () {
   return Math.round(this.duration / 7);
